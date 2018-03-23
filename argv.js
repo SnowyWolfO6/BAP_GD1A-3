f1c491583eb3e7f8fd60b194743024992afe1111
SnowyWolfO6 <greenguy666@hotmail.com>
2018-03-23 14:07:22 +0100

    argv toegevoegd

---
+++ argv.js
@@ -0,0 +1,7 @@
+console.log(process.argv);
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+console.log(process.argv[2]+process.argv[3]+process.argv[4]+process.argv[5]+process.argv[6]/5);
+console.log(6/2);
